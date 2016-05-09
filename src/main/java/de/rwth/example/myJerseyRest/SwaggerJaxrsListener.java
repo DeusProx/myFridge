88103203ec744dcf0c99980bfa815116d3fd4ec8
DeusProx <gordon.lawrenz@rwth-aachen.de>
2016-05-09 04:31:29 +0200

    initial commit

---
+++ src/main/java/de/rwth/example/myJerseyRest/SwaggerJaxrsListener.java
@@ -0,0 +1,52 @@
+package de.dbis.acis.cloud.TethysUserStorage;
+
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.wordnik.swagger.config.ConfigFactory;
+import com.wordnik.swagger.config.ScannerFactory;
+import com.wordnik.swagger.config.SwaggerConfig;
+import com.wordnik.swagger.jaxrs.config.DefaultJaxrsScanner;
+import com.wordnik.swagger.jaxrs.reader.DefaultJaxrsApiReader;
+import com.wordnik.swagger.reader.ClassReaders;
+
+/**
+ * This WebListener configures the Swagger part of this Web Application.
+ * Swagger helps exploring our API easily via a dynamic web frontend.
+ *
+ * @author Gordon Lawrenz <lawrenz@dbis.rwth-aachen.de>
+ */
+@WebListener
+public class SwaggerJaxrsListener implements ServletContextListener {
+
+	//Gets the swagger configuration from the swagger.properties
+	//ResourceBundle swaggerResourceBundle = ResourceBundle.getBundle("swagger");
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("Swagger starts...");
+		SwaggerConfig swaggerConfig = new SwaggerConfig();
+		ConfigFactory.setConfig(swaggerConfig);
+		swaggerConfig.setBasePath("http://localhost:8080/myJerseyRest");
+		swaggerConfig.setApiVersion("1.0.0");
+		ScannerFactory.setScanner(new DefaultJaxrsScanner());
+		ClassReaders.setReader(new DefaultJaxrsApiReader());
+		System.out.println("Swagger started!");
+		System.out.println("");
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.ServletContextEvent)
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("Swagger destroyed");
+	}
+
+}
---
+++ src/main/java/de/rwth/example/myJerseyRest/resources/Example.java
@@ -0,0 +1,33 @@
+package de.rwth.example.myJerseyRest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import com.wordnik.swagger.annotations.*;
+
+@Path("/example")
+@Api(value="/example", description = "Root Resource", position = 0)
+public class Example {
+
+  @GET
+  @ApiOperation(value="Says that this is the root of the web application")
+  @ApiResponses( {
+    @ApiResponse(code = 200, message = "OK")
+  } )
+  public String getMsg() {
+    String output = "This is ROOT!";
+    return output;
+  }
+
+  @GET
+  @Path("/{param}")
+  @ApiOperation(value="Says what you said")
+  @ApiResponses( {
+    @ApiResponse(code = 200, message = "OK")
+  } )
+  public String getMsg(@PathParam("param") String msg) {
+    String output = "You said: " + msg;
+    return output;
+  }
+}
