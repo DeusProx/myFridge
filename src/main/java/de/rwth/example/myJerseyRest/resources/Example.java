278a50c89e429bfcc4aaadd3dd631d6ad1ad1377
DeusProx <gordon.lawrenz@rwth-aachen.de>
2016-05-24 20:34:27 +0200

    changed api output from string type to response type with mediatype application/json

--- src/main/java/de/rwth/example/myJerseyRest/resources/Example.java
+++ src/main/java/de/rwth/example/myJerseyRest/resources/Example.java
@@ -3,6 +3,10 @@ package de.rwth.example.myJerseyRest.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.wordnik.swagger.annotations.*;
 
@@ -11,23 +15,25 @@ import com.wordnik.swagger.annotations.*;
 public class Example {
 
   @GET
+  @Produces(MediaType.APPLICATION_JSON)
   @ApiOperation(value="Says that this is the root of the web application")
   @ApiResponses( {
     @ApiResponse(code = 200, message = "OK")
   } )
-  public String getMsg() {
-    String output = "This is ROOT!";
-    return output;
+  public Response getMsg() {
+    String output = "{\"Message\":\"This is ROOT!\"}\n";
+    return Response.status(Status.OK).entity(output).build();
   }
 
   @GET
   @Path("/{param}")
+  @Produces(MediaType.APPLICATION_JSON)
   @ApiOperation(value="Says what you said")
   @ApiResponses( {
     @ApiResponse(code = 200, message = "OK")
   } )
-  public String getMsg(@PathParam("param") String msg) {
-    String output = "You said: " + msg;
-    return output;
+  public Response getMsg(@PathParam("param") String msg) {
+    String output = "{\"Message\":\"You said " + msg +"\"}\n";
+    return Response.status(Status.OK).entity(output).build();
   }
 }
